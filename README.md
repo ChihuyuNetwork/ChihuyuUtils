1806266c60069e6df9e923a952070e9cd22248ca
hirosukt <senolynn7584@gmail.com>
2023-03-15 01:42:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# PurpurPluginTemplate
+Best purpur plugin template for me.  
+※All of settings are tuned for me, probably you unfamiliar write codes at this template.
+
+## Usage
+1. Set minecraft version in `gradle.properties`
+2. Run gradle task `setup`
+3. You are the godly plugin developer
+
+## Test
+This template using syari's [minecraft-server-gradle-plugin](https://github.com/sya-ri/minecraft-server-gradle-plugin) for testing plugin.  
+So please see the their document.
+
+## Format
+You can format codes to very handsome using ktlint.
+1. Configure rules in `build.gradle.kts#ktlint`
+2. Run `ktlintFormat`
---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
+import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask.JarUrl
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("dev.s7a.gradle.minecraft.server") version "2.1.0"
+    `maven-publish`
+    `kotlin-dsl`
+}
+
+group = "love.chihuyu"
+version = "0.0.1"
+val pluginVersion: String by project.ext
+
+repositories {
+    mavenCentral()
+    maven("https://repo.codemc.org/repository/maven-public/")
+    maven("https://repo.purpurmc.org/snapshots")
+    maven("https://repo.hirosuke.me/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("org.purpurmc.purpur:purpur-api:$pluginVersion-R0.1-SNAPSHOT")
+    compileOnly("dev.jorel:commandapi-core:8.7.6")
+    compileOnly("dev.jorel:commandapi-kotlin:8.7.6")
+    implementation("org.yaml:snakeyaml:2.0")
+    implementation(kotlin("stdlib"))
+}
+
+ktlint {
+    ignoreFailures.set(true)
+    disabledRules.add("no-wildcard-imports")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+        from(sourceSets.main.get().resources.srcDirs) {
+            filter(org.apache.tools.ant.filters.ReplaceTokens::class, mapOf("tokens" to mapOf("version" to project.version.toString(), "name" to project.name)))
+            filteringCharset = "UTF-8"
+        }
+    }
+
+    shadowJar {
+        val loweredProject = project.name.lowercase()
+        dependencies {
+            include("org.jetbrains.kotlin:kotlin-stdlib")
+            include("dev.jorel:commandapi-core:8.7.6")
+            include("dev.jorel:commandapi-kotlin:8.7.6")
+        }
+        exclude("org/slf4j/**")
+        relocate("org.snakeyaml", "love.chihuyu.$loweredProject.lib.org.snakeyaml")
+        relocate("kotlin", "love.chihuyu.$loweredProject.lib.kotlin")
+        relocate("dev.jorel.commandapi", "love.chihuyu.$loweredProject.lib.dev.jorel.commandapi")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "repo"
+            credentials(PasswordCredentials::class)
+            url = uri("https://repo.hirosuke.me/repository/maven-central/")
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(18)
+}
+
+task<LaunchMinecraftServerTask>("buildAndLaunchServer") {
+    dependsOn("build")
+    doFirst {
+        copy {
+            from(buildDir.resolve("libs/${project.name}.jar"))
+            into(buildDir.resolve("server/plugins"))
+        }
+    }
+
+    jarUrl.set(JarUrl.Paper(pluginVersion))
+    jarName.set("server.jar")
+    serverDirectory.set(buildDir.resolve("server"))
+    nogui.set(true)
+    agreeEula.set(true)
+}
+
+open class SetupTask : DefaultTask() {
+
+    @TaskAction
+    fun action() {
+        val projectDir = project.projectDir
+        val srcDir = projectDir.resolve("src/main/kotlin/love/chihuyu/${project.name.lowercase()}").apply(File::mkdirs)
+        srcDir.resolve("Plugin.kt").writeText(
+            """
+                package love.chihuyu.${project.name.lowercase()}
+                
+                import org.bukkit.plugin.java.JavaPlugin
+
+                class Plugin: JavaPlugin() {
+                    companion object {
+                        lateinit var plugin: JavaPlugin
+                    }
+                
+                    init {
+                        plugin = this
+                    }
+                }
+            """.trimIndent()
+        )
+    }
+}
+
+task<SetupTask>("setup")
