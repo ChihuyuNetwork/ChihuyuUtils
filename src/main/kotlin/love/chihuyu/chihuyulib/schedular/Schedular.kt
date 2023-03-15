b9cf81ca2bc4639edef4bd332813d09b1a0155bc
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-03-15 19:14:16 +0900

    fix schedular

--- src/main/kotlin/love/chihuyu/chihuyulib/schedular/Schedular.kt
+++ src/main/kotlin/love/chihuyu/chihuyulib/schedular/Schedular.kt
@@ -1,14 +1,14 @@
 package love.chihuyu.chihuyulib.schedular
 
-import love.chihuyu.chihuyulib.ChihuyuLibPlugin
+import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitTask
 
 object Schedular {
-    fun ChihuyuLibPlugin.sync(function: () -> Unit): BukkitTask = server.scheduler.runTask(this, function)
-    fun ChihuyuLibPlugin.sync(delay: Long, function: () -> Unit): BukkitTask = server.scheduler.runTaskLater(this, function, delay)
-    fun ChihuyuLibPlugin.sync(period: Long, delay: Long = 0, function: () -> Unit): BukkitTask = server.scheduler.runTaskTimer(this, function, delay, period)
+    fun JavaPlugin.sync(function: () -> Unit): BukkitTask = server.scheduler.runTask(this, function)
+    fun JavaPlugin.sync(delay: Long, function: () -> Unit): BukkitTask = server.scheduler.runTaskLater(this, function, delay)
+    fun JavaPlugin.sync(period: Long, delay: Long = 0, function: () -> Unit): BukkitTask = server.scheduler.runTaskTimer(this, function, delay, period)
 
-    fun ChihuyuLibPlugin.async(function: () -> Unit): BukkitTask = server.scheduler.runTaskAsynchronously(this, function)
-    fun ChihuyuLibPlugin.async(delay: Long, function: () -> Unit): BukkitTask = server.scheduler.runTaskLaterAsynchronously(this, function, delay)
-    fun ChihuyuLibPlugin.async(period: Long, delay: Long = 0, function: () -> Unit): BukkitTask = server.scheduler.runTaskTimerAsynchronously(this, function, delay, period)
+    fun JavaPlugin.async(function: () -> Unit): BukkitTask = server.scheduler.runTaskAsynchronously(this, function)
+    fun JavaPlugin.async(delay: Long, function: () -> Unit): BukkitTask = server.scheduler.runTaskLaterAsynchronously(this, function, delay)
+    fun JavaPlugin.async(period: Long, delay: Long = 0, function: () -> Unit): BukkitTask = server.scheduler.runTaskTimerAsynchronously(this, function, delay, period)
 }
