a8e3f86f9b2d1a4c910a5f1f1dc75da3d69b3887
hirosukt <senolynn7584@gmail.com>
2023-07-23 15:04:11 +0900

    boom boom boooom

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.0"
+    `maven-publish`
+    `kotlin-dsl`
+}
+
+group = "love.chihuyu"
+version = "1.0.0-SNAPSHOT"
+val pluginVersion: String by project.ext
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
+}
+
+dependencies {
+    compileOnly("org.bukkit:bukkit:$pluginVersion-R0.1-SNAPSHOT")
+    compileOnly(kotlin("stdlib"))
+}
+
+ktlint {
+    ignoreFailures.set(true)
+    disabledRules.add("no-wildcard-imports")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+        from(sourceSets.main.get().resources.srcDirs) {
+            filter(
+                org.apache.tools.ant.filters.ReplaceTokens::class,
+                mapOf(
+                    "tokens" to mapOf(
+                        "version" to project.version.toString(),
+                        "name" to project.name,
+                        "mainPackage" to "love.chihuyu.${project.name.lowercase()}.${project.name}Plugin"
+                    )
+                )
+            )
+            filteringCharset = "UTF-8"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "repo"
+            credentials(PasswordCredentials::class)
+            url = uri("https://repo.hirosuke.me/snapshots/")
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+open class SetupTask : DefaultTask() {
+
+    @TaskAction
+    fun action() {
+        val projectDir = project.projectDir
+        projectDir.resolve("renovate.json").deleteOnExit()
+        val srcDir = projectDir.resolve("src/main/kotlin/love/chihuyu/${project.name.lowercase()}").apply(File::mkdirs)
+        srcDir.resolve("${project.name}Plugin.kt").writeText(
+            """
+                package love.chihuyu.${project.name.lowercase()}
+                
+                import org.bukkit.plugin.java.JavaPlugin
+
+                class ${project.name}Plugin: JavaPlugin() {
+                    companion object {
+                        lateinit var ${project.name}Plugin: JavaPlugin
+                    }
+                
+                    init {
+                        ${project.name}Plugin = this
+                    }
+                }
+            """.trimIndent()
+        )
+    }
+}
+
+task<SetupTask>("setup")
--- src/main/kotlin/love/chihuyu/chihuyusnippets/item/ItemStackBuilder.kt
+++ src/main/kotlin/love/chihuyu/chihuyusnippets/item/ItemStackBuilder.kt
@@ -4,7 +4,7 @@ import org.bukkit.Material
 import org.bukkit.event.Listener
 import org.bukkit.inventory.ItemStack
 
-class ItemStackBuilder(material: Material): ItemStack(material), Listener {
+class ItemStackBuilder(material: Material) : ItemStack(material), Listener {
 
     constructor(material: Material, builder: (ItemStack.() -> Unit) = {}) : this(material) {
         ItemStack(material).apply(builder)
